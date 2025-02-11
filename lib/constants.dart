ed43d44a0e0a79b08d6cb57e76440d35dae502d4
Kurt Cantuba <kurtbcantuba24@gmail.com>
2025-02-11 19:09:06 +0800

    s

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color FB_PRIMARY = Color(0xFF032F30);
-const Color FB_SECONDARY = Color(0xFF003bb3);
-const Color FB_DARK_PRIMARY = Color(0xFF0C969C);
-const Color FB_LIGHT_PRIMARY = Color(0xFF74b7fb);
-const Color FB_TEXT_COLOR_WHITE = Color(0xFFe6BA3BE);
